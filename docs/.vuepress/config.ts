b08b645341b90d201956e3c7e0d4599342761690
VENI-VIDIVICI <1311735850@qq.com>
2022-08-11 20:12:23 +0800

    feat 增加侧边栏

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,16 @@
 import { defineUserConfig } from 'vuepress'
-
+const { defaultTheme } = require('@vuepress/theme-default')
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
+  theme: defaultTheme({
+    navbar: [
+      // NavbarItem
+      {
+        text: '开始',
+        link: '/begin/',
+      },
+    ],
+  }),
 })
